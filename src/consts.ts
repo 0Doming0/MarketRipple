d002f7bd850d35fb0df05c1b558046b935a4ac02
0Doming0 <vitorkevin96@gmail.com>
2026-06-30 02:53:26 -0300

    First commit: estrutura do blog e ads.txt

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'MarketRipple';
+export const SITE_DESCRIPTION = 'Uncovering the stories, tech, and ripples shaping the future of money. Explore deep insights on global wealth, market trends, and crypto. Let’s talk finance.';
