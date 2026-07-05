9cb1ed135a53e81406474d58f01a7aab0998bba8
0Doming0 <vitorkevin96@gmail.com>
2026-07-05 02:21:24 -0300

    fix more stuff

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			customData: `<heroImage>${post.data.heroImage}</heroImage>`,
+			customData: `<heroImage>${post.data.heroImageData.src}</heroImage>`,
 			link: `/blog/${post.id}/`,
 		})),
 	});
