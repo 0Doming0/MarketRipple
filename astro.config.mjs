84f32fbbd23c1fe06381351a605da0cd12f79a4e
0Doming0 <vitorkevin96@gmail.com>
2026-07-08 00:01:01 -0300

    update sitemap with www

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marketripplenews.com',
+  site: 'https://www.marketripplenews.com',
   output: 'static',
   adapter: vercel(),
   integrations: [mdx(), sitemap()],
