bb13bde160e72c009778785c49a0b0c3f13b1410
0Doming0 <vitorkevin96@gmail.com>
2026-07-10 18:31:36 -0300

    update internal link

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,6 +15,7 @@ const blog = defineCollection({
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
+			postUrl: z.string().optional(),
 			heroImageData: z.object({
 				height: z.number(),
 				width: z.number(),
